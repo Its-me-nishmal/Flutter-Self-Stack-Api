2f9fc655110a65c99f53da23bce2898d129602c9
its-me-nishmal <twalaba123@gmail.com>
2023-12-13 22:19:00 +0530

    vercel host testing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = 4004
+
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Export the Express API
+module.exports = app
