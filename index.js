7bbad0b95ade245c57b51884334517463070e106
its-me-nishmal <twalaba123@gmail.com>
2023-12-14 10:43:06 +0530

    body json tested

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import connect from './database/connect.js'
 import express from 'express'
 const app = express()
+import bodyParser from 'body-parser';
 
 connect()
 import userRoutes from './routes.js/user.js';
 const PORT = 4004
 
-
+app.use(bodyParser.json());
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 })
