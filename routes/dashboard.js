18fb8227590c1ac1770c2622fdbfbbcb098d45e2
its-me-nishmal <twalaba123@gmail.com>
2024-01-26 22:59:11 +0530

    task module error fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import taskController from '../controllers/task.js';
+import taskController from '../controllers/dashboard.js';
 
 const router = express.Router();
 
 router.get('/:id', taskController.taskGet);
 router.get('/', taskController.taskGetAll);
-router.delete('/:id', taskController.taskDel);
+router.delete('/:id', taskController.taskDelete);
 router.delete('/', taskController.taskDelAll);
 router.post('/', taskController.taskCreate);
 router.put('/:id', taskController.taskUpdate);
