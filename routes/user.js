049e646a835d02f2d038ee0647948f6df672439d
its-me-nishmal <twalaba123@gmail.com>
2024-01-27 10:35:25 +0530

    auth fix

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userController from '../controllers/user.js';
-import auth from './middleware/auth.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
