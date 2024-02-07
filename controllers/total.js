66249bc395a002523c3c96c35b29c5f6ad7c0b8d
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-02-07 05:56:53 +0000

    json formatting

--- controllers/total.js
+++ controllers/total.js
@@ -12,16 +12,25 @@ export const getTotalRequests = (req, res) => {
             res.status(200).json({
                 "success": true,
                 "total_Requests": count.totalRequests,
+                "message": "🚀 Woohoo! Data successfully retrieved! Let's celebrate! 🎉",
                 "details": {
-                  "creator": "Nishmal 🚀",
-                  "license": "🔓 Free for All 🌍",
-                  "contact": {
-                    "for_api_key": "📩 Want to unlock the API magic? Drop me a message!"
-                  }
-                },
-                "message": "✨ Data retrieved successfully! 🎉"
-              }
-              );
+                    "creator": "Nishmal 🌟",
+                    "license": "🔓 Open Source - Free for All! 🌍",
+                    "documentation": {
+                        "description": "📚 Explore the API documentation to get started:",
+                        "link": "https://its-me-nishmal.github.io/Flutter-Self-Stack-Api/#/"
+                    },
+                    "github_repo": {
+                        "description": "💻 Dive deeper into the code! Check out my GitHub repository:",
+                        "link": "https://github.com/Its-me-nishmal/Flutter-Self-Stack-Api"
+                    },
+                    "contact": {
+                        "for_api_key": "💡 Want to unlock the API magic? Just drop me a message!",
+                        "email": "📧 alltrackerx@gmail.com",
+                        "whatsapp": "📲 +91-79941-07442"
+                    },
+                }
+            });
         })
         .catch(err => {
             res.status(500).json({ "success": false, "message": "Error retrieving total requests" });
