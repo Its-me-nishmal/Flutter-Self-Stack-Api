24a2142e43e6a2c1b287f5a3529891e11ad6ce3b
Its-me-nishmal-2 <160415706+Its-me-nishmal-2@users.noreply.github.com>
2024-02-22 05:43:25 +0000

    random

--- database/connect.js
+++ database/connect.js
@@ -5,7 +5,7 @@ import gradient from "gradient-string";
 const connect = async () => {  
     try {
         dotenv.config();
-        const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://alltrackerx:Nichuvdr%40786@cluster0.zqjk0it.mongodb.net/FLUTTER_SELF-STACK?retryWrites=true&w=majority'
+        const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://alltrackerx:Nichuvdr%40786@cluster0.zqjk0it.mongodb.net/FLUTTER_SELF_STACK?retryWrites=true&w=majority'
         const connetion = await mongoose.connect(MONGODB_URI)
         console.log(gradient('#02AABD', '#00CDAC','white')(`MongoDB Connected: ${connetion.connection.host}`));
     } catch (error) {console.error(gradient('#D4145A', '#FBB03B')(`Error: ${error.message}`));}
