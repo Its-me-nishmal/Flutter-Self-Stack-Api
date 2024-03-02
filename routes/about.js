047321c730bcc8df6a763ebf1a1c5cf9022b1e2b
Its-me-nishmal-2 <160415706+Its-me-nishmal-2@users.noreply.github.com>
2024-03-02 05:54:35 +0000

    test

--- routes/about.js
+++ routes/about.js
@@ -2,9 +2,9 @@ import { createAboutUs, getAboutUs, updateAboutUs } from '../controllers/aboutus
 import express from 'express';
 const router = express.Router();
 
-router.get('/about', getAboutUs)
-router.put('/about', updateAboutUs)
-router.post('/about', createAboutUs)
+router.get('/', getAboutUs)
+router.put('/', updateAboutUs)
+router.post('/', createAboutUs)
 
 
 export default router;
