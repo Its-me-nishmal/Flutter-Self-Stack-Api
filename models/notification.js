c6f8a1e525cb6f9cc3b7a273ed700a49c4293ffb
Its-me-nishmal-2 <160415706+Its-me-nishmal-2@users.noreply.github.com>
2024-03-04 06:27:26 +0000

    last

--- models/notification.js
+++ models/notification.js
@@ -1,3 +1,17 @@
-const Notification = (userId, title, body, deviceToken) => ({ userId, title, body, deviceToken });
+// notificationModel.js
+
+import mongoose from 'mongoose';
+
+// Define the schema for the Notification model
+const notificationSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  deviceToken: { type: String, required: true },
+  sentAt: { type: Date, default: Date.now }
+});
+
+// Create the Notification model
+const Notification = mongoose.model('Notification', notificationSchema);
 
 export default Notification;
