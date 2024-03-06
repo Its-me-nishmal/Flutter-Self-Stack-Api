6d2de9050439cae28442fea70ef120de4b4d5640
Its-me-nishmal-2 <160415706+Its-me-nishmal-2@users.noreply.github.com>
2024-03-06 06:37:17 +0000

    last

--- controllers/batch.js
+++ controllers/batch.js
@@ -20,6 +20,7 @@ export const getAllBatches = async (req, res) => {
 
         return res.status(200).json({ batches: batchesWithStudents });
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ error: 'Error retrieving batches and students' });
     }
 };
