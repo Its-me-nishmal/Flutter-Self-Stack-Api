5716c3cf23c84cfbf436ff01c99991f6f5595bee
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-03-15 06:17:45 +0000

    added

--- models/batch.js
+++ models/batch.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const batchSchema = new Schema({
+    _id: { type: String, default: () => `self-stack-batch-${uuidv4()}`, required: true },
     name: { type: String, required: true },
     startDate: { type: Date, required: true },
     studentIds: [{ type: String, ref: 'User' }] // Array of Student IDs referencing the User model
