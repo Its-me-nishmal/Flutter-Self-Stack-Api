e3aa795adaff3a36e82e54c34c31ba1eb6356440
Its-me-nishmal-2 <160415706+Its-me-nishmal-2@users.noreply.github.com>
2024-03-25 04:53:32 +0000

    dfj

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -25,7 +25,7 @@ export const getfeedback = async (req, res) => {
         // Fetch task names for each feedback
         const formattedFeedbacks = await Promise.all(feedbacks.map(async feedback => {
             // Find the course associated with the feedback
-            const course = await CourseModel.findOne({ tasks: feedback.taskId });
+            const course = await CourseModel.findById({ tasks: feedback.taskId });
 
             let taskName = 'Unknown Task';
             // If course is found, find the task within it
